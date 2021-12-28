7c347dc115709a87925827117ae824a41e7b4866
Tushar@8640 <imtantushar44@gmail.com>
2021-12-28 13:11:57 +0600

    all things working

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vqk54.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri =
+  "mongodb+srv://mydbuser1st:dQAzKzaqP3zjQlK3@cluster0.vqk54.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -23,10 +24,21 @@ async function run() {
     const database = client.db("CRT");
     const usersCollection = database.collection("users");
 
+    // collect user data to database api
     app.post("/addusers", async (req, res) => {
-      const result = await usersCollection.insertOne(req.body);
-      console.log(result);
-      res.send("User Successfully added")
+      const { email } = req.body.userData;
+      const allData = { ...req.body.userData };
+      const data = await usersCollection.findOne({ email: email });
+      console.log(email, data);
+      if (data) {
+        res.send("User Already Registered");
+        console.log("User Already Registered");
+      } else {
+        const result = await usersCollection.insertOne(allData);
+        console.log(result);
+        res.send("User Successfully added");
+        console.log("User Successfully added");
+      }
     });
 
     console.log("database connected");
